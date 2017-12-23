42da8045c16e7b53575b1a1f82397ca846a561ec
LaiQingFa <2511695066@qq.com>
2017-12-23 23:08:22 +0800

    创建模型

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from __future__ import unicode_literals
 
 # Create your models here.
+
+class Article(models.Model):
+    title=models.CharField(max_length=32,default='title')
+    contrnt=models.TextField(null=True)
